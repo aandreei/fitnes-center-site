a91720c91c58b0ab350bc92f951c4b4da0e137a5
aandreei <andreeisyhov@gmail.com>
2024-07-22 19:08:54 +0500

    Заканчивает верстку мобилки

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,20 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
+//видео
 document.querySelector('.about_video-overlay').onclick = function(){
   this.style.display = 'none';
 };
+
+//ограничение на цифры в инпуте с именем
+const inputName = document.getElementById('name');
+
+inputName.addEventListener('keydown', (e) =>{
+  if(e.key.match(/[0-9]/)) {
+    return e.preventDefault();
+  }
+});
+
+inputName.addEventListener('input', () =>{
+  name.value = inputName.value.replace(/[0-9]/g, '');
+});
